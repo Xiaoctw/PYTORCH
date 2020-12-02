2ba4939aff015722d8b7e01c042ea52ea037aa7e
xiaoyinuo <m18846183092@163.com>
2020-12-02 19:21:21 +0800

    更改load_data

--- GAN/utils.py
+++ GAN/utils.py
@@ -24,7 +24,7 @@ def encode_onehot(labels):
     #  output:[1, 4, 9, 16, 25]
 
 
-def load_data(path="./core/", dataset="cora"):
+def load_data(path="./cora/", dataset="cora"):
     """Load citation network dataset (cora only for now)"""
     print('Loading {} dataset...'.format(dataset))
 
