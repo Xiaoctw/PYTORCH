70b36aa7cc1bc35667be488e6435d2b9e424790c
xiaoyinuo <m18846183092@163.com>
2020-12-05 12:09:25 +0800

    更改参数

--- GAT/main.py
+++ GAT/main.py
@@ -88,8 +88,8 @@ if args.cuda:
 def compute_test():
     model.eval()
     output = model(features, adj)
-    loss_test = F.nll_loss(output[idx_test], labels[idx_test])
-    acc_test = accuracy(output[idx_test], labels[idx_test])
+    loss_test = F.nll_loss(output[idx_val], labels[idx_val])
+    acc_test = accuracy(output[idx_val], labels[idx_val])
     print("Test set results:",
           "loss= {:.4f}".format(loss_test.item()),
           "accuracy= {:.4f}".format(acc_test.item()))
